e23781dd8d8f89f86815100f4612d8aecc6deb75
BetaMedina <betameshemedina@gmail.com>
2022-09-07 02:22:12 -0300

    [Deploy] adding kubernetes files

--- bot/commands/commands.go
+++ bot/commands/commands.go
@@ -13,7 +13,7 @@ func removeIndex(s []string, index int) []string {
 }
 
 func defaultMessage(s *discordgo.Session, m *discordgo.MessageCreate) {
-	s.ChannelMessageSend(m.ChannelID, "I couldn't understand your message,please send in the same model below:\n  >>> ** !news 0 2022 fall {} **\n** !news [seasons] [year] [page] **")
+	s.ChannelMessageSend(m.ChannelID, "I couldn't understand your message,please send in the same model below:\n  >>> ** !news fall 2022 0  {} **\n** !news [seasons] [year] [page] **")
 	return
 }
 
@@ -22,17 +22,17 @@ func ExecuteCommand(s *discordgo.Session, m *discordgo.MessageCreate) {
 		return
 	}
 	message := strings.Split(strings.TrimSpace(m.Content), " ")
-	switch message[0] {
-	case "!news":
-		controller := anime.Create(s, m)
-		page := 0
-		if message[3] != "" {
-			page, _ = strconv.Atoi(string(message[1]))
-		}
-		controller.Get(message[1], message[2], page)
-		break
-	default:
-		defaultMessage(s, m)
-		break
+	if message[0] != "!news" {
+		return
 	}
+	if len(message) < 4 {
+		defaultMessage(s, m)
+		return
+	}
+	controller := anime.Create(s, m)
+	page := 0
+	if message[3] != "" {
+		page, _ = strconv.Atoi(string(message[1]))
+	}
+	controller.Get(message[1], message[2], page)
 }
